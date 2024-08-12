0affc5214937676687b9925618390640fe94a801
ThaiGiaHuy <thaigiahuy.hlk@gmail.com>
2024-08-12 10:07:52 +0700

    feat: fix pixel_wise function

--- pixel_wise_matching.py
+++ pixel_wise_matching.py
@@ -13,7 +13,7 @@ def l2_distance(x, y):
     return (x-y)**2
 
 
-def pixel_wise_matching_l1(left_img, right_img, disparity_range, save_result=True):
+def pixel_wise_matching(left_img, right_img, disparity_range, distance_method, save_result=True):
     # read left, right images then convert to grayscale
     left = cv2.imread(left_img, 0)
     right = cv2.imread(right_img, 0)
@@ -24,14 +24,21 @@ def pixel_wise_matching_l1(left_img, right_img, disparity_range, save_result=Tru
 
     # initial depth matrix
     h, w = left.shape[:2]
-    max_value = 255
+    if distance_method == l1_distance:
+        max_value = 255
+    elif distance_method == l2_distance:
+        max_value = 255 ** 2
+    else:
+        max_value = 0
     scale = 16
 
+    # calculate cost using broadcasting
     cost = np.full((h, w, disparity_range), max_value, dtype=np.float32)
     for d in range(disparity_range):
         left_d = left[:, d:]
         right_d = right[:, 0:w-d]
-        cost[:, d:, d] = l1_distance(left_d, right_d)
+        # using distance_method: l1, l2
+        cost[:, d:, d] = distance_method(left_d, right_d)
 
     min_cost = np.argmin(cost, axis=2)
     depth = min_cost * scale
@@ -40,43 +47,10 @@ def pixel_wise_matching_l1(left_img, right_img, disparity_range, save_result=Tru
     if save_result == True:
         print('Saving result...')
         # Save results
-        cv2.imwrite(f'{path}pixel_wise_l1.png', depth)
-        cv2.imwrite(f'{path}/pixel_wise_l1_color.png',
-                    cv2.applyColorMap(depth, cv2.COLORMAP_JET))
+        end_name = "l1" if distance_method == l1_distance else "l2"
 
-    print('Done.')
-    return depth
-
-
-def pixel_wise_matching_l2(left_img, right_img, disparity_range, save_result=True):
-    # read left, right images then convert to grayscale
-    left = cv2.imread(left_img, 0)
-    right = cv2.imread(right_img, 0)
-
-    # convert to float32
-    left = left.astype(np.float32)
-    right = right.astype(np.float32)
-
-    # initial depth matrix
-    h, w = left.shape[:2]
-    max_value = 255
-    scale = 16
-
-    cost = np.full((h, w, disparity_range), max_value, dtype=np.float32)
-    for d in range(disparity_range):
-        left_d = left[:, d:]
-        right_d = right[:, 0:w-d]
-        cost[:, d:, d] = l2_distance(left_d, right_d)
-
-    min_cost = np.argmin(cost, axis=2)
-    depth = min_cost * scale
-    depth = depth.astype(np.uint8)
-
-    if save_result == True:
-        print('Saving result...')
-        # Save results
-        cv2.imwrite(f'{path}pixel_wise_l2.png', depth)
-        cv2.imwrite(f'{path}pixel_wise_l2_color.png',
+        cv2.imwrite(f'{path}pixel_wise_{end_name}.png', depth)
+        cv2.imwrite(f'{path}pixel_wise_{end_name}_color.png',
                     cv2.applyColorMap(depth, cv2.COLORMAP_JET))
 
     print('Done.')
@@ -87,12 +61,14 @@ left_img_path = 'assets/tsukuba/left.png'
 right_img_path = 'assets/tsukuba/right.png'
 disparity_range = 16
 
-pixel_wise_result_l1 = pixel_wise_matching_l1(left_img_path,
-                                              right_img_path,
-                                              disparity_range,
-                                              save_result=True)
+pixel_wise_result_l1 = pixel_wise_matching(left_img_path,
+                                           right_img_path,
+                                           disparity_range,
+                                           l1_distance,
+                                           save_result=True)
 
-pixel_wise_result_l2 = pixel_wise_matching_l2(left_img_path,
-                                              right_img_path,
-                                              disparity_range,
-                                              save_result=True)
+pixel_wise_result_l2 = pixel_wise_matching(left_img_path,
+                                           right_img_path,
+                                           disparity_range,
+                                           l2_distance,
+                                           save_result=True)
---
+++ window_based_matching.py
@@ -0,0 +1,86 @@
+import cv2
+import numpy as np
+
+path = 'result/window_based/'
+
+
+def l1_distance(x, y):
+    return abs(x-y)
+
+
+def l2_distance(x, y):
+    return (x-y)**2
+
+
+def window_based_matching(left_img, right_img, disparity_range, distance_method, kernel_size=5, save_result=True):
+    # read images
+    left = cv2.imread(left_img, 0)
+    right = cv2.imread(right_img, 0)
+
+    left = left.astype(np.float32)
+    right = right.astype(np.float32)
+
+    #define depth matrix and max value
+    height, width = left.shape[:2]
+    depth = np.zeros((height, width), dtype=np.uint8)
+    if distance_method == l1_distance:
+        max_value = 255*9
+    elif distance_method == l2_distance:
+        max_value = 255**2*9
+    else:
+        max_value = 1e7
+    scale = 3
+
+    # kernel half: distance from center to the edge
+    kernel_half = int((kernel_size - 1)/2)
+    for y in range(kernel_half, height - kernel_half + 1):
+        for x in range(kernel_half, width - kernel_half + 1):
+            disparity = 0
+            cost_min = max_value
+            for d in range(disparity_range):
+                window_cost = 0
+                # calculate 1 cost
+                for v in range(-kernel_half, kernel_half+1):
+                    for u in range(-kernel_half, kernel_half+1):
+                        if (x+u-d) < 0:
+                            cost = max_value
+                        else:
+                            cost = distance_method(
+                                left[y+v, x+u], right[y+v, x+u-d])
+                        window_cost += cost
+                if window_cost < cost_min:
+                    cost_min = window_cost
+                    disparity = d
+            depth[y, x] = disparity * scale
+
+    if save_result == True:
+        print('Saving result...')
+        # Save results
+        end_name = "l1" if distance_method == l1_distance else "l2"
+
+        cv2.imwrite(f'{path}window_based_{end_name}.png', depth)
+        cv2.imwrite(f'{path}window_based_{end_name}_color.png',
+                    cv2.applyColorMap(depth, cv2.COLORMAP_JET))
+
+    print('Done.')
+    return depth
+
+
+left_img_path = 'assets/Aloe/Aloe_left_1.png'
+right_img_path = 'assets/Aloe/Aloe_right_1.png'
+disparity_range = 64
+kernel_size = 3
+
+window_based_result_l1 = window_based_matching(left_img_path,
+                                               right_img_path,
+                                               disparity_range,
+                                               l1_distance,
+                                               kernel_size,
+                                               save_result=True)
+
+window_based_result_l2 = window_based_matching(left_img_path,
+                                               right_img_path,
+                                               disparity_range,
+                                               l1_distance,
+                                               kernel_size,
+                                               save_result=True)
